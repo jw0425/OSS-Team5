49f7a193a5c4c6deac0257414f529b80bc12f7c2
GitKosy <tjsduq0527@naver.com>
2019-11-23 21:43:46 +0900

    기능수정 : 난이도 변경 기능 수정
    변경 전
    - 난이도 변경 기능이 runGameVSCom 함수에 포함되어 있으며, 유지보수적
    측면에서 좋지 않다.
    변경 후
    - computerLvlSelect 함수를 생성하여 기능의 분할을 하였다.

--- FuncDeclare.h
+++ FuncDeclare.h
@@ -11,15 +11,14 @@ void makeMove(int[][A_SIZE], int, int, const int);
 int is3inARow(const int[][A_SIZE], const int);
 int isAWin(const int[][A_SIZE], const int);
 
-void HumanTurn(int[][A_SIZE]);
-void runGame(void);
 
 int minMax_EASY(int[][A_SIZE], int  );
 int minMax_NORMAL(int[][A_SIZE], int , int* );
 int minMax_HARD(int[][A_SIZE], int, int*);
 void ComputerTurn(int[][A_SIZE], int , int );
+void HumanTurn(int[][A_SIZE]);
 int Max(int[], int, int[], int*);
 int Min(int[], int, int[], int*);
-
+int computerLvlSelect();
 
 #endif
